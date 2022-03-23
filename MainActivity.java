293b26a7c098a1f521bd22162ef90a83ee18aaf4
Shivaniswami2 <101874706+Shivaniswami2@users.noreply.github.com>
2022-03-23 22:56:37 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,30 +1,65 @@
-package com.firstapp.imagerslider;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.firstapp.navigation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
 
-import com.denzcoskun.imageslider.ImageSlider;
-import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firstapp.navigation.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        ImageSlider imageSlider = findViewById(R.id.slider);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
 
-        List<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel("https://media-exp1.licdn.com/dms/image/C561BAQH65fZfm_SyWg/company-background_10000/0/1594625804077?e=2147483647&v=beta&t=-1mxRi7gTusVj6uL3BS9KHrfRYsgiGk5DdJnovZ3rGU","1 Image"));
-        slideModels.add(new SlideModel("https://careerworldedu.org/storage/files/in/2200/thumb-816x460-28ae5272f8f469db11d6794ca541ba15.jpg","2 Image"));
-        slideModels.add(new SlideModel("https://images.static-collegedunia.com/public/college_data/images/campusimage/15857152962.JPG","3 Image"));
-        slideModels.add(new SlideModel("https://careerworldedu.org/storage/files/in/2200/thumb-816x460-9a7f3d554319cd1304efbb48da6b9c06.jpg","4 Image"));
-        imageSlider.setImageList(slideModels, true );
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
